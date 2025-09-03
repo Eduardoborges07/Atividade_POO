61cbfbcf1a08f4666aca606170eabf515fb4a256
Eduardo Borges Lemos <eduardoborgeslemos2007@gmail.com>
2025-09-03 10:31:33 -0300

    Update Animal.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,4 +1,46 @@
 
-public class Animal {
+public abstract class Animal {
 
-}
+	private String nome;
+	private int idade;
+	private double peso;
+	private String habitat;
+	
+	public Animal(String nome, int idade, double peso, String habitat) {
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+        this.habitat = habitat;
+    }
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public double getPeso() {
+		return peso;
+	}
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	public String getHabitat() {
+		return habitat;
+	}
+	public void setHabitat(String habitat) {
+		this.habitat = habitat;
+	}	  
+		
+     public void emitirSom() {
+		        System.out.println("Animal emitindo Som");
+     }
+     public void alimentar() {
+	        System.out.println("Animal se alimentando");
+     }
