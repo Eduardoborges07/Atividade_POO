4af1a5db9d33618c1ee8038f10ce0843cf51eee2
Eduardo Borges Lemos <eduardoborgeslemos2007@gmail.com>
2025-09-03 10:33:07 -0300

    Update Cachorro.java

--- src/Cachorro.java
+++ src/Cachorro.java
@@ -1,4 +1,14 @@
 
-public class Cachorro {
+public class Cachorro extends Animal {
+	private String raca;
 
+    public Cachorro(String nome, int idade, double peso, String habitat, String raca) {
+        super(nome, idade, peso, habitat);
+        this.raca = raca;
+    public void emitirSom() {
+	        System.out.println("Latido");
+ }
+ public void alimentar() {
+        System.out.println("Ração");
+ }
 }
