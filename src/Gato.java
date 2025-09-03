3d5ae60d942c12466c7febf1ba0937e511b17ff9
Eduardo Borges Lemos <eduardoborgeslemos2007@gmail.com>
2025-09-03 10:34:07 -0300

    Update Gato.java

--- src/Gato.java
+++ src/Gato.java
@@ -1,4 +1,16 @@
 
-public class Gato {
+public class Gato extends Animal {
+	private String cor;
+
+    public Gato(String nome, int idade, double peso, String habitat, String cor) {
+        super(nome, idade, peso, habitat);
+        this.cor = cor;
+    }
+    public void emitirSom() {
+        System.out.println("Miau");
+    }
+    public void alimentar() {
+    System.out.println("Ração de gato");
+    }
 
 }
